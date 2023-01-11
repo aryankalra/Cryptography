a2d4e6881fcc2e07da79f596536e65da8b64e6ec
aryankalra <68978161+aryankalra@users.noreply.github.com>
2023-01-11 17:54:49 +1100

    Hashcode Test

---
+++ JHTest.java
@@ -0,0 +1,15 @@
+
+public class JHTest {
+
+	public static void main(String[] args) {
+		String as = "mpE";
+		String bs = "nQE";
+		String cs = "nPd";
+
+		System.out.println("Hashcode for " + as + " = " + as.hashCode());
+		System.out.println("Hashcode for " + bs + " = " + bs.hashCode());
+		System.out.println("Hashcode for " + cs + " = " + cs.hashCode());
+
+	}
+
+}
